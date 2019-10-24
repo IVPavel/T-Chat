8ddb29aada2d001584e88d3a299c95944dc40869
Pavel Ivanov <pavel.ivanov.1995@gmail.com>
2019-10-24 16:10:25 +0300

    Fixed display of the keyboard on the login screen.

--- T-Chat/Controllers/LoginViewController.swift
+++ T-Chat/Controllers/LoginViewController.swift
@@ -13,6 +13,7 @@ class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,6 +28,38 @@ class LoginViewController: UIViewController {
         
         loginTextField.text = ""
         passwordTextField.text = ""
+        
+        registerKeyboardNotification()
+        //scrollView.contentOffset = CGPoint(x: 0, y: 123)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        
+        removeKeyboardNotification()
+    }
+    
+    func registerKeyboardNotification() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification: NSNotification) {
+        guard let keyboardFrame = notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue else { return }
+        let keyboardSize = keyboardFrame.cgRectValue
+        
+        scrollView.contentSize = CGSize(width: self.view.bounds.size.width, height: view.bounds.size.height - keyboardSize.height)
+        scrollView.contentOffset = CGPoint(x: 0, y: keyboardSize.height)
+    }
+    
+    @objc func keyboardWillHide(_ notification: NSNotification) {
+        scrollView.contentSize = CGSize(width: self.view.bounds.size.width, height: view.bounds.size.height)
+        scrollView.contentOffset = CGPoint.zero
+    }
+    
+    func removeKeyboardNotification() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
     }
 
     @IBAction func loginButton(_ sender: UIButton) {
